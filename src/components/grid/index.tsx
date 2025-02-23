c591234b49c9138a9b616df79355386e3f77539f
devesh711 <pdevesh699@gmail.com>
2025-02-24 04:24:12 +0530

    ADD Grid data ans Types

---
+++ src/components/grid/index.tsx
@@ -0,0 +1,298 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import cn from "classnames";
+import styles from "./grid.module.scss";
+import {
+  WidthProvider,
+	ResponsiveGridLayout as Responsive,
+} from "react-grid-layout-next";
+import { heights, layouts } from "@/data";
+import ThemeToggle from "../theme-toggle";
+import BlurFade from "../magic-ui/blur-fade";
+import {
+	TextMarqueeCard,
+	MyTechCard,
+	JobsCard,
+	AboutCard,
+	ResumeCard,
+	IconCard,
+	ProjectCard,
+	SkillChartCard
+} from "@/components/cards";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+const Grid = () => {
+	const [height, setHeight] = useState(280);
+	const [isMounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setMounted(true);
+		}, 500);
+	}, []);
+
+	return (
+		<div className={styles.container}>
+			<ResponsiveGridLayout
+				useCSSTransforms
+				className={styles.layout}
+				layouts={layouts.all}
+				breakpoints={{
+					lg: 1199,
+					md: 799,
+					sm: 374,
+				}}
+				cols={{
+					lg: 12,
+					md: 10,
+					sm: 4,
+				}}
+				isDraggable={true}
+				rowHeight={height}
+				onBreakpointChange={(breakpoint: any) => {
+					setHeight(heights[breakpoint])
+				}}
+				margin={[16, 16]}
+			>
+				<div
+					key="bio"
+					className={cn(styles.itemGrab, styles.item)}
+					style={{
+						transitionDuration: isMounted ? "500ms" : "0ms",
+						height: "100%"
+					}}>
+					<BlurFade delay={0.01}>
+						<AboutCard />
+					</BlurFade>
+				</div>
+        <div
+					key="theme-toggle"
+					className={cn(styles.itemGrab, styles.item)}
+					style={{
+						transitionDuration: isMounted ? "500ms" : "0ms"
+					}}>
+            <div className="flex flex-col-reverse sm:flex-col gap-3 h-full">
+							<BlurFade delay={0.1}>
+								<ResumeCard />
+							</BlurFade>
+							<BlurFade delay={0.12}>
+								<ThemeToggle />
+							</BlurFade>
+            </div>
+				</div>
+				<div
+					key="skill"
+					className={cn(styles.itemGrab, styles.item)}
+					style={{
+						transitionDuration: isMounted ? "500ms" : "0ms"
+					}}
+				>
+					<BlurFade delay={0.08}>
+						<SkillChartCard />
+					</BlurFade>
+				</div>
+        <div
+					key="text-marquee"
+					className={cn(styles.itemGrab, styles.item)}
+					style={{
+						transitionDuration: isMounted ? "500ms" : "0ms"
+					}}
+				>
+					<BlurFade delay={0.04}>
+						<TextMarqueeCard />
+					</BlurFade>
+				</div>
+        <div
+          key="linkedin"
+          className={cn(styles.itemGrab, styles.item)}
+          style={{
+            transitionDuration: isMounted ? "500ms" : "0ms"
+          }}
+        >
+					<BlurFade delay={0.24}>
+						<IconCard title="Linkedin" link="https://www.linkedin.com/in/PatelDevesh/" />
+					</BlurFade>
+        </div>
+        <div
+          key="github"
+          className={cn(styles.itemGrab, styles.item)}
+          style={{
+            transitionDuration: isMounted ? "500ms" : "0ms"
+          }}
+        >
+					<BlurFade delay={0.26}>
+						<IconCard title="Github" link="https://github.com/devesh711" />
+					</BlurFade>
+        </div>
+        <div
+          key="codepen"
+          className={cn(styles.itemGrab, styles.item)}
+          style={{
+            transitionDuration: isMounted ? "500ms" : "0ms"
+          }}
+        >
+					<BlurFade delay={0.28}>
+						<IconCard title="Codepen" link="https://medium.com/@devesh711" />
+					</BlurFade>
+        </div>
+        <div
+          key="email"
+          className={cn(styles.itemGrab, styles.item)}
+          style={{
+            transitionDuration: isMounted ? "500ms" : "0ms"
+          }}
+        >
+					<BlurFade delay={0.30}>
+						<IconCard title="Email" link="mailto:pdevesh699+work@gmail.com" />
+					</BlurFade>
+        </div>
+        <div
+					key="my-stack"
+					className={cn(styles.itemGrab, styles.item)}
+					style={{
+						transitionDuration: isMounted ? "500ms" : "0ms"
+					}}
+        >
+					<BlurFade delay={0.35}>
+						<MyTechCard />
+					</BlurFade>
+				</div>
+        <div
+					key="jobs"
+					className={cn(styles.itemGrab, styles.item)}
+					style={{
+						transitionDuration: isMounted ? "700ms" : "0ms"
+					}}
+        >
+					<BlurFade delay={0.40}>
+						<JobsCard />
+					</BlurFade>
+				</div>
+				<div
+					key="neurogen"
+					className={cn(styles.itemGrab, styles.item)}
+					style={{
+						transitionDuration: isMounted ? "700ms" : "0ms"
+					}}
+        >
+					<BlurFade delay={0.50}>
+					<ProjectCard 
+						    projectImage="/projects/neurogen.png"
+							projectAlt="Neurogen"
+							companyLogo="/companies/logo_neurogen.png"
+							companyAlt="Neurogen logo"
+							companyName="Neurogen"
+							projectTitle="Neuorgen"
+							projectLink="https://neurogen.app/portfolio"
+							badges={["Backend + Deployment", "React", "Django", "REST API", "Postgres","AWS"]}
+						/>
+					</BlurFade>
+				</div>
+				<div
+					key="Simulation-Request-Portal"
+					className={cn(styles.itemGrab, styles.item)}
+					style={{
+						transitionDuration: isMounted ? "700ms" : "0ms"
+					}}
+        >
+					<BlurFade delay={0.55}>
+					<ProjectCard 
+						      projectImage="/projects/SRP.svg"
+							  projectAlt="Simulation Request Portal"
+							  companyLogo="/companies/apollo_solo_logo.png"
+							  companyAlt="Apollo logo"
+							  companyName="Apollo Tyres"
+							  projectTitle="Simulation Request Portal"
+							  projectLink="/"
+							  badges={["FullStack", "Django", "Celery", "Postgres"]}
+					/>
+					</BlurFade>
+				</div>
+				<div
+					key="health-insight"
+					className={cn(styles.itemGrab, styles.item)}
+					style={{
+						transitionDuration: isMounted ? "700ms" : "0ms"
+					}}
+        >
+					<BlurFade delay={0.60}>
+					<ProjectCard 
+						    projectImage="/projects/RetroRevive.png"
+							projectAlt="Health Insight"
+							companyLogo="/companies/Noirinsight.jpeg"
+							companyAlt="Noirinsight logo"
+							companyName="NoirInsight"
+							projectTitle="Health Insight"
+							projectLink="https://github.com/NOIRINSIGHT/Health-Insight"
+							badges={["Frontend + Backend", "svelte", "OpenAi", "Google TTS"]}
+						/>
+					</BlurFade>
+				</div>
+				<div
+					key="retro-revive"
+					className={cn(styles.itemGrab, styles.item)}
+					style={{
+						transitionDuration: isMounted ? "700ms" : "0ms"
+					}}
+        >
+					<BlurFade delay={0.60}>
+					<ProjectCard 
+						    projectImage="/projects/RetroRevive.png"
+							projectAlt="Retro Revive"
+							companyLogo="/companies/Noirinsight.jpeg"
+							companyAlt="Noirinsight logo"
+							companyName="NoirInsight"
+							projectTitle="Retro Revive"
+							projectLink="https://github.com/NOIRINSIGHT/Retro-Revive"
+							badges={["Frontend + Backend", "Django", "opencv", "AWS S3"]}
+						/>
+					</BlurFade>
+				</div>
+				{/* <div
+					key="alliance-landing"
+					className={cn(styles.itemGrab, styles.item)}
+					style={{
+						transitionDuration: isMounted ? "700ms" : "0ms"
+					}}
+        >
+					<BlurFade delay={0.70}>
+						<ProjectCard 
+						    projectImage="/projects/Alliance-money.png"
+							projectAlt="Alliance landing page example"
+							companyLogo="/companies/01_Terra_color.png"
+							companyAlt="TFL logo"
+							companyName="Terraform Labs"
+							projectTitle="Alliance Landing"
+							projectLink="https://alliance.money/"
+							badges={["Frontend + Design", "React", "Typescript", "SCSS"]}
+						/>
+					</BlurFade>
+				</div>
+				<div
+					key="developer-landing"
+					className={cn(styles.itemGrab, styles.item)}
+					style={{
+						transitionDuration: isMounted ? "700ms" : "0ms"
+					}}
+        >
+					<BlurFade delay={0.75}>
+					<ProjectCard 
+						      projectImage="/projects/Alliance-money.png"
+							  projectAlt="Alliance landing page example"
+							  companyLogo="/companies/01_Terra_color.png"
+							  companyAlt="TFL logo"
+							  companyName="Terraform Labs"
+							  projectTitle="Alliance Landing"
+							  projectLink="https://alliance.money/"
+							  badges={["Frontend + Design", "React", "Typescript", "SCSS"]}
+						/> 
+					</BlurFade>
+				</div> */}
+			</ResponsiveGridLayout>
+		</div>
+	);
+};
+
+export default Grid;
---
+++ src/data/index.tsx
@@ -0,0 +1,761 @@
+export const filters = ["All", "About", "Work"];
+
+export const layouts = {
+  all: {
+    lg: [ // > 1216px
+      {
+        i: "bio",
+        x: 0, y: 0, w: 5, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "theme-toggle",
+        x: 9, y: 1, w: 4, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "skill",
+        x: 5, y: 1, w: 3, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "text-marquee",
+        x: 5, y: 0, w: 7, h: 1,
+        isResizable: false,
+      },
+      {
+        i: "linkedin",
+        x: 0, y: 4, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "github",
+        x: 0, y: 6, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "codepen",
+        x: 0, y: 8, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "email",
+        x: 2, y: 8, w: 4, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "my-stack",
+        x: 2, y: 4, w: 4, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "jobs",
+        x: 6, y: 4, w: 6, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "neurogen",
+        x: 0, y: 10, w: 6, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "Simulation-Request-Portal",
+        x: 6, y: 8, w: 6, h: 7,
+        isResizable: false,
+      },
+      {
+        i: "retro-revive",
+        x: 0, y: 16, w: 6, h: 7,
+        isResizable: false,
+      },
+      {
+        i: "health-insight",
+        x: 6, y: 15, w: 6, h: 7,
+        isResizable: false,
+      },
+      {
+        i: "alliance-landing",
+        x: 0, y: 23, w: 6, h: 7,
+        isResizable: false,
+      },
+      {
+        i: "developer-landing",
+        x: 6, y: 22, w: 6, h: 7,
+        isResizable: false,
+      },
+    ],
+    md: [ // 814px - 1215px
+      {
+        i: "bio",
+        x: 0, y: 0, w: 5, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "theme-toggle",
+        x: 7, y: 1, w: 5, h: 5,
+        isResizable: false,
+      },
+      {
+        i: "skill",
+        x: 8, y: 6, w: 2, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "text-marquee",
+        x: 5, y: 0, w: 5, h: 1,
+        isResizable: false,
+      },
+      {
+        i: "linkedin",
+        x: 4, y: 6, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "github",
+        x: 6, y: 6, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "codepen",
+        x: 4, y: 8, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "email",
+        x: 6, y: 8, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "my-stack",
+        x: 0, y: 6, w: 4, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "jobs",
+        x: 1, y: 12, w: 8, h: 7,
+        isResizable: false,
+      },
+      {
+        i: "neurogen",
+        x: 0, y: 19, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "Simulation-Request-Portal",
+        x: 6, y: 19, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "retro-revive",
+        x: 0, y: 27, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "health-insight",
+        x: 5, y: 27, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "alliance-landing",
+        x: 0, y: 35, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "developer-landing",
+        x: 5, y: 35, w: 5, h: 8,
+        isResizable: false,
+      },
+    ],
+    sm: [ // 0px - 813px
+      {
+        i: "bio",
+        x: 0, y: 1, w: 4, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "theme-toggle",
+        x: 0, y: 7, w: 4, h: 5,
+        isResizable: false,
+      },
+      {
+        i: "skill",
+        x: 0, y: 12, w: 4, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "text-marquee",
+        x: 0, y: 0, w: 4, h: 1,
+        isResizable: false,
+      },
+      {
+        i: "linkedin",
+        x: 0, y: 16, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "github",
+        x: 2, y: 16, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "codepen",
+        x: 0, y: 19, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "email",
+        x: 2, y: 19, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "my-stack",
+        x: 0, y: 22, w: 4, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "jobs",
+        x: 0, y: 28, w: 4, h: 9,
+        isResizable: false,
+      },
+      {
+        i: "neurogen",
+        x: 0, y: 37, w: 6, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "Simulation-Request-Portal",
+        x: 0, y: 47, w: 6, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "health-insight",
+        x: 0, y: 57, w: 6, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "retro-revive",
+        x: 0, y: 67, w: 6, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "alliance-landing",
+        x: 0, y: 77, w: 6, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "developer-landing",
+        x: 0, y: 87, w: 6, h: 10,
+        isResizable: false,
+      },
+    ],
+  },
+  about: {
+    lg: [
+      {
+        i: "bio",
+        x: 0, y: 0, w: 5, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "theme-toggle",
+        x: 7, y: 5, w: 5, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "skill",
+        x: 9, y: 0, w: 3, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "text-marquee",
+        x: 7, y: 4, w: 5, h: 1,
+        isResizable: false,
+      },
+      {
+        i: "linkedin",
+        x: 5, y: 0, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "github",
+        x: 7, y: 0, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "codepen",
+        x: 5, y: 2, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "email",
+        x: 7, y: 2, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "my-stack",
+        x: 0, y: 4, w: 7, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "jobs",
+        x: 6, y: 8, w: 6, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "neurogen",
+        x: 0, y: 8, w: 6, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "Simulation-Request-Portal",
+        x: 0, y: 14, w: 6, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "health-insight",
+        x: 6, y: 15, w: 6, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "retro-revive",
+        x: 0, y: 20, w: 6, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "alliance-landing",
+        x: 6, y: 21, w: 6, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "developer-landing",
+        x: 0, y: 26, w: 6, h: 6,
+        isResizable: false,
+      },
+    ],
+    md: [
+      {
+        i: "bio",
+        x: 0, y: 0, w: 5, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "theme-toggle",
+        x: 6, y: 6, w: 4, h: 5,
+        isResizable: false,
+      },
+      {
+        i: "skill",
+        x: 9, y: 0, w: 3, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "text-marquee",
+        x: 6, y: 11, w: 4, h: 1,
+        isResizable: false,
+      },
+      {
+        i: "linkedin",
+        x: 5, y: 0, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "github",
+        x: 5, y: 2, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "codepen",
+        x: 5, y: 4, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "email",
+        x: 7, y: 0, w: 3, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "my-stack",
+        x: 0, y: 6, w: 6, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "jobs",
+        x: 1, y: 12, w: 8, h: 7,
+        isResizable: false,
+      },
+      {
+        i: "neurogen",
+        x: 0, y: 22, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "Simulation-Request-Portal",
+        x: 5, y: 22, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "health-insight",
+        x: 0, y: 30, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "retro-revive",
+        x: 5, y: 30, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "alliance-landing",
+        x: 0, y: 38, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "developer-landing",
+        x: 5, y: 38, w: 5, h: 8,
+        isResizable: false,
+      },
+    ],
+    sm: [
+      {
+        i: "bio",
+        x: 0, y: 0, w: 4, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "theme-toggle",
+        x: 0, y: 23, w: 4, h: 5,
+        isResizable: false,
+      },
+      {
+        i: "skill",
+        x: 0, y: 6, w: 4, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "text-marquee",
+        x: 0, y: 22, w: 4, h: 1,
+        isResizable: false,
+      },
+      {
+        i: "linkedin",
+        x: 0, y: 10, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "github",
+        x: 2, y: 10, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "codepen",
+        x: 0, y: 13, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "email",
+        x: 2, y: 13, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "my-stack",
+        x: 0, y: 16, w: 4, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "jobs",
+        x: 0, y: 28, w: 6, h: 9,
+        isResizable: false,
+      },
+      {
+        i: "neurogen",
+        x: 0, y: 44, w: 6, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "Simulation-Request-Portal",
+        x: 0, y: 54, w: 6, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "health-insight",
+        x: 0, y: 64, w: 6, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "retro-revive",
+        x: 0, y: 74, w: 6, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "alliance-landing",
+        x: 0, y: 84, w: 6, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "developer-landing",
+        x: 0, y: 94, w: 6, h: 10,
+        isResizable: false,
+      },
+    ],
+  },
+  work: {
+    lg: [
+      {
+        i: "bio",
+        x: 6, y: 21, w: 6, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "theme-toggle",
+        x: 8, y: 0, w: 6, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "skill",
+        x: 9, y: 25, w: 3, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "text-marquee",
+        x: 0, y: 29, w: 12, h: 1,
+        isResizable: false,
+      },
+      {
+        i: "linkedin",
+        x: 0, y: 25, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "github",
+        x: 2, y: 25, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "codepen",
+        x: 0, y: 27, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "email",
+        x: 2, y: 25, w: 2, h: 2,
+        isResizable: false,
+      },
+      {
+        i: "my-stack",
+        x: 4, y: 25, w: 5, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "jobs",
+        x: 0, y: 0, w: 6, h: 4,
+        isResizable: false,
+      },
+      {
+        i: "neurogen",
+        x: 6, y: 3, w: 6, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "Simulation-Request-Portal",
+        x: 0, y: 7, w: 6, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "retro-revive",
+        x: 6, y: 9, w: 6, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "health-insight",
+        x: 0, y: 13, w: 6, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "alliance-landing",
+        x: 6, y: 15, w: 6, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "developer-landing",
+        x: 0, y: 19, w: 6, h: 6,
+        isResizable: false,
+      },
+    ],
+    md: [
+      {
+        i: "bio",
+        x: 5, y: 29, w: 5, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "theme-toggle",
+        x: 5, y: 0, w: 5, h: 5,
+        isResizable: false,
+      },
+      {
+        i: "skill",
+        x: 8, y: 36, w: 2, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "text-marquee",
+        x: 5, y: 35, w: 5, h: 1,
+        isResizable: false,
+      },
+      {
+        i: "linkedin",
+        x: 0, y: 36, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "github",
+        x: 2, y: 36, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "codepen",
+        x: 0, y: 39, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "email",
+        x: 2, y: 39, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "my-stack",
+        x: 4, y: 36, w: 4, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "jobs",
+        x: 0, y: 0, w: 5, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "neurogen",
+        x: 0, y: 12, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "Simulation-Request-Portal",
+        x: 5, y: 5, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "retro-revive",
+        x: 5, y: 13, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "health-insight",
+        x: 0, y: 20, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "alliance-landing",
+        x: 5, y: 21, w: 5, h: 8,
+        isResizable: false,
+      },
+      {
+        i: "developer-landing",
+        x: 0, y: 28, w: 5, h: 8,
+        isResizable: false,
+      },
+    ],
+    sm: [
+      {
+        i: "bio",
+        x: 0, y: 82, w: 4, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "theme-toggle",
+        x: 0, y: 0, w: 4, h: 5,
+        isResizable: false,
+      },
+      {
+        i: "skill",
+        x: 0, y: 100, w: 4, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "text-marquee",
+        x: 0, y: 81, w: 4, h: 1,
+        isResizable: false,
+      },
+      {
+        i: "linkedin",
+        x: 0, y: 94, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "github",
+        x: 2, y: 94, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "codepen",
+        x: 0, y: 97, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "email",
+        x: 2, y: 97, w: 2, h: 3,
+        isResizable: false,
+      },
+      {
+        i: "my-stack",
+        x: 0, y: 88, w: 4, h: 6,
+        isResizable: false,
+      },
+      {
+        i: "jobs",
+        x: 0, y: 5, w: 4, h: 9,
+        isResizable: false,
+      },
+      {
+        i: "neurogen",
+        x: 0, y: 21, w: 4, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "Simulation-Request-Portal",
+        x: 0, y: 31, w: 4, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "retro-revive",
+        x: 0, y: 41, w: 4, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "health-insight",
+        x: 0, y: 51, w: 4, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "alliance-landing",
+        x: 5, y: 61, w: 4, h: 10,
+        isResizable: false,
+      },
+      {
+        i: "developer-landing",
+        x: 0, y: 71, w: 4, h: 10,
+        isResizable: false,
+      },
+    ],
+  },
+};
+
+export const heights: { [key: string]: number } = {
+  // lg: 280,
+  // lg: 70,
+  lg: 58,
+  // md: 180,
+  md: 33,
+  // sm: 164,
+  sm: 29,
+  // xs: 136,
+};
+
+export const memojis = ["images/memoji-1.png", "images/memoji-2.png"];
