b10b88705d0f10391e00137e44944379ca48795f
devesh711 <pdevesh699@gmail.com>
2025-02-24 04:23:51 +0530

    ADD Index for comps

---
+++ src/components/cards/index.tsx
@@ -0,0 +1,11 @@
+
+export { default as CardWrapper } from "./card-wrapper";
+export { default as ResumeCard } from "./resume-card";
+export { default as IconCard } from "./icon-card";
+export { default as JobsCard } from "./jobs-card";
+export { default as AboutCard } from "./about-card";
+export { default as MyTechCard } from "./tech-card";
+export { default as TextMarqueeCard } from "./text-marquee-card";
+export { default as ProjectCard } from "./project-card";
+export { default as SkillChartCard } from "./skill-chart-card";
+
